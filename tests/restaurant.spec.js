fa5534247bb4f2942cf9051af7dcb9f91e21bc29
devarturmartins <arturvl1@hotmail.com>
2022-09-21 20:42:48 -0300

    tentando r. 10

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -54,8 +54,9 @@ const createMenu = require('../src/restaurant');
 
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // TESTE 1: Verifique se função `createMenu()` retorna um objeto que possui a chave `fetchMenu`, a qual tem como valor uma função.
+    expect(typeof createMenu()[0]).toBe('fetchMenu');
     // ```
     // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
     // ```
